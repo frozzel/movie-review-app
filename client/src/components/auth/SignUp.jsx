0c55dadbdeeb034f11d4f0fdbe9923e2cca577cd
Dennis Hickox <frozzel@me.com>
2023-04-17 13:57:59 -0400

    created sign In and Up forms

--- client/src/components/auth/SignUp.jsx
+++ client/src/components/auth/SignUp.jsx
@@ -4,18 +4,19 @@ import Title from "../user/form/Title";
 import FormInput from "../user/form/FormInput";
 import Submit from "../user/form/Submit";
 
-export default function SignIn() {
+export default function SignUp() {
   return (
     <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
       <Container>
         <form className="bg-secondary rounded p-6 w-72 space-y-6">
-          <Title >Sign In</Title>
+          <Title >Sign Up</Title>
+          <FormInput label="Name" name="name"  placeholder="Your Name" />
           <FormInput label="Email" name="email"  placeholder="your@email.com" />
           <FormInput label="Password" name="password"  placeholder="********" />
-          <Submit value="Sign In"></Submit>
+          <Submit value="Sign Up"></Submit>
           <div className="flex justify-between">
             <a className="text-dark-subtle hover:text-white" href="#">Forget Password</a>
-            <a className="text-dark-subtle hover:text-white" href="#">Sign Up</a>
+            <a className="text-dark-subtle hover:text-white" href="#">Sign In</a>
           </div>
         </form>
       </Container>

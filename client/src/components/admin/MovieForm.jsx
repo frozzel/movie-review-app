b97402197decbb958e4d293066fa98add66f1c56
Dennis Hickox <frozzel@me.com>
2023-05-06 14:16:16 -0400

    created customizable live search to allow searching from with in any form section

--- client/src/components/admin/MovieForm.jsx
+++ client/src/components/admin/MovieForm.jsx
@@ -1,12 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import { commonInputClasses } from "../../utils/theme";
+import LiveSearch from "../LiveSearch";
 import TagsInput from "../TagsInput";
 
-export const commonInputClasses =
-  "w-full bg-transparent outline-none dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary transition dark:text-white text-primary";
+export const results = [
+  {
+    id: "1",
+    avatar:
+      "https://images.unsplash.com/photo-1643713303351-01f540054fd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "John Doe",
+  },
+  {
+    id: "2",
+    avatar:
+      "https://images.unsplash.com/photo-1643883135036-98ec2d9e50a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Chandri Anggara",
+  },
+  {
+    id: "3",
+    avatar:
+      "https://images.unsplash.com/photo-1578342976795-062a1b744f37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Amin RK",
+  },
+  {
+    id: "4",
+    avatar:
+      "https://images.unsplash.com/photo-1564227901-6b1d20bebe9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Edward Howell",
+  },
+  {
+    id: "5",
+    avatar:
+      "https://images.unsplash.com/photo-1578342976795-062a1b744f37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Amin RK",
+  },
+  {
+    id: "6",
+    avatar:
+      "https://images.unsplash.com/photo-1564227901-6b1d20bebe9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Edward Howell",
+  },
+];
+
+const defaultMovieInfo = {
+  title: "",
+  storyLine: "",
+  tags: [],
+  cast: [],
+  director: {},
+  writers: [],
+  releaseDate: "",
+  poster: null,
+  genres: [],
+  type: "",
+  language: "",
+  status: "",
+};
+
 export default function MovieForm() {
+  const [movieInfo, setMovieInfo] = useState({ ...defaultMovieInfo });
   const handleSubmit = (e) => {
     e.preventDefault();
   };
+
+  const renderItem = (result) => {
+    return (
+      <div className="flex rounded overflow-hidden">
+        <img src={result.avatar} alt="" className="w-16 h-16 object-cover" />
+        <p className="dark:text-white font-semibold">{result.name}</p>
+      </div>
+    );
+  };
+
+  const handleChange = ({ target }) => {
+    const { value, name } = target;
+
+    setMovieInfo({ ...movieInfo, [name]: value });
+  };
+
+  const { title, storyLine, director } = movieInfo;
 
   return (
     <form onSubmit={handleSubmit} className="flex space-x-3">
@@ -15,6 +87,9 @@ export default function MovieForm() {
           <Label htmlFor="title">Title</Label>
           <input
             id="title"
+            value={title}
+            onChange={handleChange}
+            name="title"
             type="text"
             className={commonInputClasses + " border-b-2 font-semibold text-xl"}
             placeholder="Titanic"
@@ -24,15 +99,26 @@ export default function MovieForm() {
         <div>
           <Label htmlFor="storyLine">Plot</Label>
           <textarea
+            value={storyLine}
+            onChange={handleChange}
+            name="storyLine"
             id="storyLine"
             className={commonInputClasses + " border-b-2 resize-none h-24"}
             placeholder="Movie storyline..."
           ></textarea>
         </div>
+
         <div>
-            <Label htmlFor="tags">Tags</Label>
-            <TagsInput />
+          <Label htmlFor="tags">Tags</Label>
+          <TagsInput />
         </div>
+
+        <LiveSearch
+          results={results}
+          placeholder="Search profile"
+          renderItem={renderItem}
+          onSelect={(result) => console.log(result)}
+        />
       </div>
       <div className="w-[30%] h-5 bg-blue-400"></div>
     </form>
